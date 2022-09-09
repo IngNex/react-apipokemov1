26291c576b913ffde88e72bfdd5ab2755d125856
IngNex <ingnexmaicol14@gmail.com>
2022-09-08 21:15:41 -0500

    updating react-apipokemo adding find pokemon

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
- 
-
-
-
- 
-
-
-
-
 #!/usr/bin/env sh
 
 # abort on errors
@@ -52,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:ingnex/react-apipokemov1.git main:gh-pages
+# git push -f git@github.com:ingnex/react-apipokemov2.git main:gh-pages
 
 cd -
